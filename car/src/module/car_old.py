89afe623444e47583cfe5b900d389e76e98a9180
fuyuan360 <2259582806@qq.com>
2025-07-25 20:47:13 +0800

    chore: 新增.gitignore及汽车模块基础文件 添加忽略虚拟环境和IDE配置，新增Car类实现及旧版本对比文件

---
+++ car/src/module/car_old.py
@@ -0,0 +1,18 @@
+# Author fuyuan: fuyuan360@qq.com
+class Car :
+    def __init__(self, make, model, **kwargs) :
+        self.make = make
+        self.model = model
+        self.attributes = kwargs
+
+    def __getattr__(self, arg) :
+        if arg in self.attributes :
+            return self.attributes[arg]
+        else :
+            raise AttributeError(f"Car object has no attribute '{arg}'")
+
+
+# 示例
+ford = Car("Ford", "F150", color="red")
+print(ford.color)
+print(ford.aer)
